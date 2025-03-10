b12a9e096560e68c6bdc5464148131b2ddcc5c8c
Darya Korbut <kitkatlolipop@mail.ru>
2025-03-10 17:46:02 +0300

    fix: remove babel builder, fix eslint config

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,8 +5,6 @@ import { BuildOptions } from './types/types';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-import path from 'path';
-import CopyPlugin from 'copy-webpack-plugin';
 
 export function buildPlugins({
   mode,
@@ -20,7 +18,6 @@ export function buildPlugins({
   const plugins: Configuration['plugins'] = [
     new HtmlWebpackPlugin({
       template: paths.html,
-      favicon: path.resolve(paths.public, 'home.ico'),
     }),
     new DefinePlugin({
       __PLATFORM__: JSON.stringify(platform),
@@ -30,7 +27,6 @@ export function buildPlugins({
 
   if (isDev) {
     plugins.push(new webpack.ProgressPlugin());
-    /** Выносит проверку типов в отдельный процесс: не нагружая сборку */
     plugins.push(new ForkTsCheckerWebpackPlugin());
     plugins.push(new ReactRefreshWebpackPlugin());
   }
@@ -40,16 +36,6 @@ export function buildPlugins({
       new MiniCssExtractPlugin({
         filename: 'css/[name].[contenthash:8].css',
         chunkFilename: 'css/[name].[contenthash:8].css',
-      })
-    );
-    plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.resolve(paths.public, 'locales'),
-            to: path.resolve(paths.output, 'locales'),
-          },
-        ],
       })
     );
   }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,13 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import eslintReactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-config-prettier';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default tseslint.config(
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     settings: {
@@ -12,15 +15,44 @@ export default [
         version: 'detect',
       },
     },
+    ignores: ['node_modules', '/build'],
   },
-  { languageOptions: { globals: globals.browser } },
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      'react-hooks': eslintReactHooks,
+      'react-refresh': eslintReactRefresh,
+      react: eslintPluginReact,
+      prettier: prettierPlugin,
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: ['tsconfig.json'],
+    },
+  },
   {
     rules: {
       'no-console': 'error',
+      'prefer-const': 'error',
       'react/no-direct-mutation-state': 'error',
+      'react/jsx-no-target-blank': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+      ...prettierPlugin.rules,
     },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+  eslintPluginReact.configs.flat.recommended
+);
