0043bb5daf0246737a510c30d97bed7793fba092
Darya Korbut <kitkatlolipop@mail.ru>
2025-03-12 13:27:18 +0300

    refactor: add some dependencies, add mocks for styles

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.scss$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.css$': '<rootDir>/__mocks__/styleMock.js',
+  },
+};
