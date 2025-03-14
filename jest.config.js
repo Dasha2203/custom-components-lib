6feeac5d3abe29b4bb19c589e6164268ae96b555
Darya Korbut <kitkatlolipop@mail.ru>
2025-03-14 13:32:23 +0300

    fix: change mapping in jest config to handle aliases

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
     '\\.scss$': '<rootDir>/__mocks__/styleMock.js',
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
   },
